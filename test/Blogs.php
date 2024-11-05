a2b8965232690eee239285877d1fdd173003df5c
Randilachathuranga <randilachathuranga100@gmail.com>
2024-11-05 21:30:49 +0530

    added

---
+++ test/Blogs.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blogs</title>
+    <link rel="stylesheet" href="./Blogstyles.css">
+</head>
+<body>
+<div class="header-container">
+    <h1 class="header-title">Blogs</h1>
+    <img src="./blogs1.png" alt="Class Image" class="class-image">
+</div>
+
+    <button class="create-blog-button" onclick="createBlog()">Create a Blog</button>
+    <!-- <button class="refresh-button">Refresh</button> -->
+
+<div class="container" id="class-container">
+    <!-- Blog cards will be appended here by JavaScript -->
+</div>
+
+
+
+<script src="./Blogscript.js"></script>
+</body>
+</html>
