054b26261fdb34ec15d8ca5cf1007bbdc9b59efe
Randilachathuranga <randilachathuranga100@gmail.com>
2024-11-06 16:38:45 +0530

    added

--- Teacher/AllAdvertisements/AllAdvertisements.php
+++ Teacher/AllAdvertisements/AllAdvertisements.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Blogs</title>
-    <link rel="stylesheet" href="./AllAddsstyles.css">
+    <link rel="stylesheet" href="./AllAdsstyles.css">
+    <link rel="stylesheet" href="./CreateAds/CreateAds.css">
 </head>
 <body>
 <div class="header-container">
@@ -12,13 +13,34 @@
     <img src="./add.png" alt="Class Image" class="class-image">
 </div>
 
-    <button class="create-blog-button" onclick="createAdd()">Create a Advertisement</button>
+    <button class="create-blog-button" onclick="createAdds()">Create a Advertisement</button>
     <!-- <button class="refresh-button">Refresh</button> -->
 
 <div class="container" id="class-container">
     <!-- Blog cards will be appended here by JavaScript -->
 </div>
 
-<script src="./AllAddsscript.js"></script>
+<!-- Popup Form for Creating a Blog -->
+<div id="popupForm" class="popup-form" style="display: none;">
+    <div class="form-container">
+        <h2>Create a Advertisement</h2>
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" required>
+        
+        <label for="image" class="custom-file-upload">Choose Image</label>
+        <input type="file" id="image" name="image" required>
+
+        <br><br>
+        <label for="description">Description</label>
+        <textarea id="description" name="description" required></textarea>
+        
+        <button onclick="submitAds()" class="submit-button">Submit</button>
+        <button onclick="closePopup()" class="close-button">Close</button>
+    </div>
+</div>
+
+
+<script src="./AllAdsscript.js"></script>
+<script src="./CreateAds/CreateAds.js"></script>
 </body>
 </html>
