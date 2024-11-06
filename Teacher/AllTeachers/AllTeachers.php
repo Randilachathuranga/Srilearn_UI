6b516c549b40b297049563519f5c98807a6d18e2
Randilachathuranga <randilachathuranga100@gmail.com>
2024-11-06 23:04:57 +0530

    hi

--- Teacher/AllTeachers/AllTeachers.php
+++ Teacher/AllTeachers/AllTeachers.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Class Cards</title>
     <link rel="stylesheet" href="./AllteachStyles.css"> <!-- Link your CSS file -->
+    <link rel="stylesheet" href="./ViewTeacher//ViewTeacher.css">
 </head>
 <body>
 <div class="header-container">
@@ -31,10 +32,35 @@
 </div>
 
 
+<!-- Popup Form for Editing a Blog -->
+<div id="popupForm" class="popup-form" style="display: none;">
+  <div class="form-container">
+    <form id="blogForm">
+      <div class="teacher-details">
+        <img src="./teachers_images//3.jpg" alt="Teacher Image" id="teacher-image" class="teacher-image">
+        <p><strong>Name:</strong> <span id="teacher-name"></span></p>
+        <p><strong>Subject:</strong> <span id="teacher-subject"></span></p>
+        <p><strong>Phone number:</strong> <span id="teacher-phone"></span></p>
+        <p><strong>Email:</strong> <span id="teacher-email"></span></p>
+        <p><strong>Institute:</strong> <span id="teacher-institute"></span></p>
+        <p><strong>Address:</strong> <span id="teacher-address"></span></p>
+
+        <p></p><strong>Qualifications:</strong> <span id="teacher-qualifications"></span></p>
+        
+      </div>
+    </form>
+    <button type="button" class="close-button" onclick="closePopup()"></button>
+  </div>
+  
+</div>
+
+
+
 
     <div class="container" id="class-container">
         <!-- Cards will be appended here -->
     </div>
     <script src="./Allteachcript.js"></script> <!-- Link your JavaScript file -->
+    <script src="./ViewTeacher/ViewTeachers.js"></script>
 </body>
 </html>
