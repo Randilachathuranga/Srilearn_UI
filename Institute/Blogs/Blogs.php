dfd69296b1f1e7b2f2256dc5a37ee4f63161b547
Dulshan201 <dulshangunarathna78@gmail.com>
2024-11-10 23:11:26 +0530

    Update Blogs.php

--- Institute/Blogs/Blogs.php
+++ Institute/Blogs/Blogs.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blogs</title>
+    <title>Blogs</title>
     <link rel="stylesheet" href="./Blogstyles.css">
     <link rel="stylesheet" href="./CreateBlogs/CreateBlogs.css">
 
