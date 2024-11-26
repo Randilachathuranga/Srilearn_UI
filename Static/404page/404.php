aadb3ed93f299e87923a292f37a35f484822f373
Randilachathuranga <randilachathuranga100@gmail.com>
2024-11-26 11:58:24 +0530

    add 404 page

---
+++ Static/404page/404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Page Not Found</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left-section">
+            <h1>Oops! <br>404 Error</h1>
+            <p>The page you are looking for does not exist. It might have been moved or deleted.</p>
+            <a href="" class="home-button">Go Back to Homepage</a>
+        </div>
+        <div class="right-section">
+            <img src="./404.png" alt="404 Illustration" class="illustration">
+        </div>
+    </div>
+</body>
+</html>
